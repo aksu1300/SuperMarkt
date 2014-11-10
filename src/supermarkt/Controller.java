b4fb630a416c108d977f6410a205a26ddf64559a
GoTr00t <aksuumit@hotmail.com>
2014-11-10 19:55:05 +0100

    Hibernate configuratie bestand aangepast voor username en password zodat iedereen de zelfde omgeving kan gebruiken. De Controller class leeg gehaald en deze in SuperMarkt class toegevoegd.

--- src/supermarkt/Controller.java
+++ src/supermarkt/Controller.java
@@ -9,20 +9,6 @@
 
 public class Controller {
 
-	public int openKassa() {
-		return 0;
-	}
 
-	public double afrekenenArtikel() {
-		return 0;
-	}
-
-	public int boodschappenVoltooid() {
-		return 0;
-	}
-
-	public void vakkenVullen() {
-
-	}
 
 }
--- src/supermarkt/SuperMarkt.java
+++ src/supermarkt/SuperMarkt.java
@@ -4,6 +4,8 @@
  */
 package supermarkt;
 
+import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -12,7 +14,26 @@ import org.hibernate.Transaction;
  * @author account
  */
 public class SuperMarkt {
+    
 
+        // Methode om een nieuwe kassa te openenen
+    	public int openKassa() {
+		return 0;
+	}
+        
+	public double afrekenenArtikel() {
+		return 0;
+	}
+
+	public int boodschappenVoltooid() {
+		return 0;
+	}
+
+	public void vakkenVullen() {
+
+	}
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -20,24 +41,40 @@ public class SuperMarkt {
         // TODO code application logic here
         System.out.printf("Welkom to Aldi\n");
         
-        // Nieuw artikel 
-        Artikel tomaat = new Artikel();
-        tomaat.setNaam("Tomaat");
-        tomaat.setPrijs(0.13f);
+//        // Nieuw artikel 
+//        Artikel tomaat = new Artikel();
+//        tomaat.setNaam("kaas");
+//        tomaat.setPrijs(1.13f);
+//
+//        // Create a new session
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        // Create a transaction
+//        Transaction tx = session.beginTransaction();
+//
+//        // Save the nieuwe artikel
+//        session.save(tomaat);
+//
+//        // Commit the db changes
+//        tx.commit();;
+//        
+//        System.out.println("We hebben een nieuwe artikel toegevoegd");
+//        
         
-        // Create a new session
+        // Now get the artikels from the db
         Session session = HibernateUtil.getSessionFactory().openSession();
         // Create a transaction
         Transaction tx = session.beginTransaction();
         
-        // Save the nieuwe artikel
-        session.save(tomaat);
+        // Create a query from the artikel class
+        Query query = session.createQuery("from Artikel");
+        List<Artikel> artikelList = query.list();
         
-        // Commit the db changes
-        tx.commit();;
         
-        System.out.println("We hebben een nieuwe artikel toegevoegd");
-        
+        System.out.println(artikelList.size() + " artikelen gevonden");
+        for(Artikel item : artikelList)
+        {
+            System.out.println("|" +item.getId() +"|"+ item.getNaam() +"|"+ item.getPrijs() +"|");
+        }
         
     }
 }
