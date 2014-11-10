e53a371053675bdd96d2a5b1e3fe5c3c6723e13a
GoTr00t <aksuumit@hotmail.com>
2014-11-10 12:20:24 +0100

    Hibernate toegevoegt en daarnaast de artikel klasse aangemaakt aan de hand van de database. De oude ArtkilS klass  is verwijderd.

--- src/supermarkt/SuperMarkt.java
+++ src/supermarkt/SuperMarkt.java
@@ -4,6 +4,9 @@
  */
 package supermarkt;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author account
@@ -15,6 +18,26 @@ public class SuperMarkt {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.printf("Hello world\n");
+        System.out.printf("Welkom to Aldi\n");
+        
+        // Nieuw artikel 
+        Artikel tomaat = new Artikel();
+        tomaat.setNaam("Tomaat");
+        tomaat.setPrijs(0.13f);
+        
+        // Create a new session
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        // Create a transaction
+        Transaction tx = session.beginTransaction();
+        
+        // Save the nieuwe artikel
+        session.save(tomaat);
+        
+        // Commit the db changes
+        tx.commit();;
+        
+        System.out.println("We hebben een nieuwe artikel toegevoegd");
+        
+        
     }
 }
